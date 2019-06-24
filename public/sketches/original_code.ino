c6519fefe0152b144e37e6f27018b9454e67dd44
Till Schander <tillschander@gmx.de>
2019-06-24 23:43:03 +0200

    New arduino sketch

--- public/sketches/original_code.ino
+++ public/sketches/original_code.ino
@@ -1,28 +1,17 @@
-// Based on https://forum.pjrc.com/threads/27082-Teensy-3-1-with-Glediator-and-Jinx-over-USB
-
 #include "FastLED.h"
 #define NUM_LEDS 64
+#define DATA_PIN 6
 
 CRGB leds[NUM_LEDS];
 
-int serialGlediator() {
-    while (!Serial.available()) {}
-    return Serial.read();
-}
-
 void setup() {
     Serial.begin(1000000);
-    LEDS.addLeds<WS2812B,6>(leds, NUM_LEDS);
+    FastLED.addLeds<WS2812B, DATA_PIN>(leds, NUM_LEDS);
 }
 
 void loop() {
-    while (serialGlediator() != 255) {} // Each frame begins with 255
-
-    for (int i=0; i < NUM_LEDS; i++) {
-        leds[i].r = serialGlediator();
-        leds[i].g = serialGlediator();
-        leds[i].b = serialGlediator();
-    }
-
-    FastSPI_LED.show();
+    while (!Serial.available()) {}
+    while (Serial.read() != 255) {}
+    Serial.readBytes((char*)leds, NUM_LEDS * 3);
+    FastLED.show();
 }
